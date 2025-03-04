7f282a3c31189ee9d73acce6451df59bb19f5588
waqas2714 <waqasali00123@gmail.com>
2025-03-04 23:20:10 +0500

    successfully triggered the workflow from react app

--- k8-auto-fe/src/App.jsx
+++ k8-auto-fe/src/App.jsx
@@ -5,11 +5,8 @@ function App() {
   const octokit = new Octokit({ auth: import.meta.env.VITE_GITHUB_PAT });
 
   const workflowDispatch = async ()=>{
-    await octokit.request('POST /repos/waqas2714/k8-automate/actions/workflows/github-actions-demo.yml/dispatches', {
-      owner: 'waqas2714',
-      repo: 'k8-automate',
-      workflow_id: 'github-actions-demo.yml',
-      ref: 'topic-branch',
+    const response = await octokit.request('POST /repos/waqas2714/k8-automate/actions/workflows/github-actions-demo.yml/dispatches', {
+      ref: 'main',
       inputs: {
         greeting: 'Hello from REST API :)'
       },
@@ -17,11 +14,14 @@ function App() {
         'X-GitHub-Api-Version': '2022-11-28'
       }
     })
+
+    console.log(response);
+    
   }
 
   return (
     <>
-      <h1>{import.meta.env.VITE_GITHUB_PAT}</h1>
+      <button onClick={workflowDispatch}>DISPATCH!!!</button>
     </>
   )
 }
