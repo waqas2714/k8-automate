35fa446f3a30854b85f1846f50c759873a969c5e
waqas2714 <waqasali00123@gmail.com>
2025-03-10 23:02:14 +0500

    clerk used

--- k8-auto-fe/src/App.jsx
+++ k8-auto-fe/src/App.jsx
@@ -1,18 +1,30 @@
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/clerk-react";
 import { Octokit } from "@octokit/core";
 
 function App() {
+  const { user } = useUser()
 
+  user && console.log(user);
+  
   const octokit = new Octokit({ auth: import.meta.env.VITE_GITHUB_PAT });
 
   const workflowDispatch = async ()=>{
-    const response = await octokit.request('POST /repos/waqas2714/k8-automate/actions/workflows/main-wf.yml/dispatches', {
+    const currentTime = new Date().toISOString();
+    const response = await octokit.request('POST /repos/waqas2714/k8-automate/actions/workflows/polling-test.yml/dispatches', {
       ref: 'main',
-      inputs: {
-        greeting: 'Hello from REST API :)',
-        awsAccessKey: import.meta.env.VITE_AWS_ACCESS_KEY,
-        awsSecretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
-        ec2_count: '5'
-      },
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+    
+    console.log(response);
+    
+    console.log("workflow run dispatched on: " + currentTime);
+    
+  }
+
+  const getWorkflowRun = async ()=>{
+    const response = await octokit.request('GET /repos/waqas2714/k8-automate/actions/workflows/polling-test.yml/runs', {
       headers: {
         'X-GitHub-Api-Version': '2022-11-28'
       }
@@ -23,9 +35,16 @@ function App() {
   }
 
   return (
-    <>
+    <div className="bg-yellow-400">
+      <SignedOut>
+        <SignInButton  />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <button onClick={workflowDispatch}>DISPATCH!!!</button>
-    </>
+      <button onClick={getWorkflowRun}>Get Workflow</button>
+    </div>
   )
 }
 
