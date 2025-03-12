26b2bad217c6e0579ccb091ec6e4552bcf671b51
waqas2714 <waqasali00123@gmail.com>
2025-03-12 23:35:03 +0500

    adding dynamic names in the first step of workflows

--- k8-auto-fe/src/App.jsx
+++ k8-auto-fe/src/App.jsx
@@ -5,13 +5,10 @@ function App() {
   const octokit = new Octokit({ auth: import.meta.env.VITE_GITHUB_PAT });
 
   const workflowDispatch = async ()=>{
-    const response = await octokit.request('POST /repos/waqas2714/k8-automate/actions/workflows/main-wf.yml/dispatches', {
-      ref: 'main',
+    const response = await octokit.request('POST /repos/waqas2714/k8-automate/actions/workflows/polling-test.yml/dispatches', {
+      ref: 'workflow-by-dynamic-step',
       inputs: {
-        greeting: 'Hello from REST API :)',
-        awsAccessKey: import.meta.env.VITE_AWS_ACCESS_KEY,
-        awsSecretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
-        ec2_count: '5'
+        step1_name: "waqas2714"
       },
       headers: {
         'X-GitHub-Api-Version': '2022-11-28'
@@ -21,8 +18,6 @@ function App() {
     console.log(response);
     
   }
-
-
 
   
   const getWorkflowRun = async () => {
