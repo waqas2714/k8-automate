ae6a160d475df173a520e4026ab57816617f341c
waqas2714 <waqasali00123@gmail.com>
2025-03-12 23:33:00 +0500

    added get workflow function

--- k8-auto-fe/src/App.jsx
+++ k8-auto-fe/src/App.jsx
@@ -22,9 +22,27 @@ function App() {
     
   }
 
+
+
+  
+  const getWorkflowRun = async () => {
+    try {
+      const response = await octokit.request(
+        "GET /repos/waqas2714/k8-automate/actions/workflows/polling-test.yml/runs",
+        {
+          headers: { "X-GitHub-Api-Version": "2022-11-28" },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.error("Error getting workflow run:", error);
+    }
+  };
+
   return (
     <>
       <button onClick={workflowDispatch}>DISPATCH!!!</button>
+      <button onClick={getWorkflowRun} className="mx-6">Get Workflow Runs</button>
     </>
   )
 }
