8fc3b90d9773e9bd59430a5a40464d47fd9105f1
waqas2714 <waqasali00123@gmail.com>
2025-04-11 01:08:03 +0500

    frontend for status completed

--- k8-auto-fe/src/WorkflowStatus.jsx
+++ k8-auto-fe/src/WorkflowStatus.jsx
@@ -6,14 +6,115 @@ function WorkflowStatus() {
   const octokit = new Octokit({ auth: import.meta.env.VITE_GITHUB_PAT });
   const userId = localStorage.getItem("uniqueUserId");
 
+  const [dummySteps, setDummySteps] = useState([
+    {
+      name: "Set up job",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "waqas2714",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Checkout code",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Generate RSA SSH Key",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Make Scripts Executable",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Check and Terminate EC2 Instances if Exists",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Check and Delete S3 Bucket if Exists",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Check and Delete Security Groups if Exists",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Check and Delete EC2 Key Pair if Exists",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Install Terraform",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Terraform Init and Apply",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Generate Ansible Inventory",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Disable Strict Host Key Checking",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Add IPs to known_hosts",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Ping Instances (Retry if Failed)",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Run Ansible Playbook",
+      status: "completed",
+      conclusion: "cancelled",
+    },
+    {
+      name: "Post Checkout code",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Post waqas2714",
+      status: "completed",
+      conclusion: "success",
+    },
+    {
+      name: "Complete job",
+      status: "completed",
+      conclusion: "success",
+    },
+  ]);
+
   const [steps, setSteps] = useState([]);
   const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [failed, setFailed] = useState(false);
   const pollingRef = useRef(null); // Track interval
 
   useEffect(() => {
-    setLoading(true);
+    console.log(steps);
+  }, [steps]);
 
+  useEffect(() => {
     // Start polling after an initial delay of 25 seconds
     const timeout = setTimeout(() => {
       getWorkflowRun();
@@ -34,70 +135,54 @@ function WorkflowStatus() {
       setSteps([]);
       setMessage("");
 
-      const savedJobId = localStorage.getItem("savedJobId");
+      const response = await octokit.request(
+        "GET /repos/waqas2714/k8-automate/actions/workflows/main-wf.yml/runs",
+        { headers: { "X-GitHub-Api-Version": "2022-11-28" } }
+      );
 
-      if (savedJobId) {
-        // If we have a saved job ID, fetch the job directly
-        const jobResponse = await octokit.request(
-          `GET /repos/waqas2714/k8-automate/actions/jobs/${savedJobId}`,
+      const workflowRuns = response.data.workflow_runs;
+      if (!workflowRuns || workflowRuns.length === 0) {
+        setMessage("No jobs found.");
+        return;
+      }
+
+      let foundSteps = [];
+      let foundJobId = null;
+
+      // Iterate through the workflow runs to find the correct job
+      for (const run of workflowRuns) {
+        const jobsResponse = await octokit.request(
+          `GET /repos/waqas2714/k8-automate/actions/runs/${run.id}/jobs`,
           { headers: { "X-GitHub-Api-Version": "2022-11-28" } }
         );
-        processJobSteps(jobResponse.data);
-      } else {
-        // If no job ID is saved, we need to look through the runs
-        const response = await octokit.request(
-          "GET /repos/waqas2714/k8-automate/actions/workflows/main-wf.yml/runs",
-          { headers: { "X-GitHub-Api-Version": "2022-11-28" } }
-        );
 
-        const workflowRuns = response.data.workflow_runs;
-        if (!workflowRuns || workflowRuns.length === 0) {
-          setMessage("No jobs found.");
-          return;
-        }
+        const jobs = jobsResponse.data.jobs;
+        if (jobs.length > 0) {
+          for (const job of jobs) {
+            const steps = job.steps;
 
-        let foundSteps = [];
-        let foundJobId = null;
-
-        // Iterate through the workflow runs to find the correct job
-        for (const run of workflowRuns) {
-          const jobsResponse = await octokit.request(
-            `GET /repos/waqas2714/k8-automate/actions/runs/${run.id}/jobs`,
-            { headers: { "X-GitHub-Api-Version": "2022-11-28" } }
-          );
-          
-          const jobs = jobsResponse.data.jobs;
-          if (jobs.length > 0) {
-            for (const job of jobs) {
-              const steps = job.steps;
-
-              // Check if the second step's name matches the uniqueUserId
-              const step = steps[1]; // Assuming step[1] is the one you want
-              if (step && step.name === userId) {
-                foundJobId = job.id;
-                foundSteps = steps.map((step) => ({
-                  name: step.name,
-                  status: step.status,
-                  conclusion: step.conclusion, // Use conclusion instead of status
-                }));
-                break;
-              }
+            // Check if the second step's name matches the uniqueUserId
+            const step = steps[1]; // Assuming step[1] is the one you want
+            if (step && step.name === userId) {
+              foundJobId = job.id;
+              foundSteps = steps.map((step) => ({
+                name: step.name,
+                status: step.status,
+                conclusion: step.conclusion, // Use conclusion instead of status
+              }));
+              break;
             }
           }
-
-          if (foundJobId) break; // Break out of the loop if we found the job
         }
 
-        // If we found the job and steps, store the job ID and show the steps
-        if (foundSteps.length > 0) {
-          setSteps(foundSteps);
-          localStorage.setItem("savedJobId", foundJobId);
+        if (foundJobId) break; // Break out of the loop if we found the job
+      }
 
-          processJobSteps({ steps: foundSteps });
-
-        } else {
-          setMessage("No jobs found.");
-        }
+      // If we found the job and steps, store the job ID and show the steps
+      if (foundSteps.length > 0) {
+        processJobSteps({ steps: foundSteps });
+      } else {
+        setMessage("No jobs found.");
       }
     } catch (error) {
       console.error("Error getting workflow run:", error);
@@ -116,15 +201,16 @@ function WorkflowStatus() {
     setSteps(steps);
 
     // Check for any failed steps based on conclusion
-    const failed = steps.find((step) => step.conclusion === "failure");
+    const failed = jobData.steps.find((step) => step.conclusion === "failure");
     if (failed) {
       // If a failure is found, set the status of all remaining steps to 'canceled'
+
       const updatedSteps = steps.map((step, index) => {
         if (index === steps.indexOf(failed)) {
-          return { ...step, status: 'failed' };
+          return { ...step, status: "failed" };
         }
         if (index > steps.indexOf(failed)) {
-          return { ...step, status: 'canceled' };
+          return { ...step, status: "canceled" };
         }
         return step;
       });
@@ -137,42 +223,63 @@ function WorkflowStatus() {
         pollingRef.current = null;
       }
 
-      setLoading(false);
-
       // Optionally, you can show a toast or alert here to notify the user
-      toast.error(`Error found in step: ${failed.name}. All subsequent steps have been canceled.`);
-    } else {
-      // If no failure, clear previous error-related data
-      localStorage.removeItem("workflow_dispatched");
-      localStorage.removeItem("failed_step");
+      toast.error(
+        `Error found in step: ${failed.name}. All subsequent steps have been canceled.`
+      );
+
+      setFailed(true);
+    }
+    setLoading(false);
+  };
+
+  const getStepBackgroundColor = (status) => {
+    switch (status) {
+      case "pending":
+        return "bg-[#0D70E0]"; // Blue for pending
+      case "completed":
+        return "bg-[#2EB54D]"; // Green for completed
+      case "failed":
+        return "bg-[#B72B00]"; // Red for failed
+      case "canceled":
+        return "bg-[#949494]"; // Gray for canceled
+      default:
+        return "bg-gray-400"; // Default fallback color
     }
   };
 
   return (
-    <div className="p-4">
+    <div className={`bg-[#25292E] min-h-screen w-screen ${!loading && 'py-12'}`}>
       {loading && (
-        <div className="mt-4 text-center">
-          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-blue-500 border-solid"></div>
-          <p className="text-gray-500">Loading...</p>
+        <div className="text-center h-screen w-full flex flex-col space-y-16 justify-center items-center">
+          <div className="animate-spin rounded-full h-14 w-14 border-t-2 border-[#F7F7F7] border-solid"></div>
+          <p className="text-[#F7F7F7] text-lg ">Your progress will be showed soon!</p>
         </div>
       )}
 
       {!loading && steps.length > 0 && (
-        <div className="mt-4 p-4 border border-gray-300 rounded">
-          <h2 className="font-bold text-lg">Matched Workflow Steps:</h2>
-          <ul className="list-disc ml-6">
-            {steps.map((step, index) => (
-              <li key={index}>
-                <span className="font-medium">{step.name}</span> -{" "}
-                <span className={`text-${step.status === "failed" ? "red" : step.status === "canceled" ? "yellow" : "gray"}-600`}>
-                  {step.status}
-                </span>
-              </li>
-            ))}
-          </ul>
+        <div className="p-4 w-[95%] mx-auto">
+          <h2 className="font-bold text-2xl text-white">Progress:</h2>
+          <div className="flex flex-wrap space-y-5 space-x-2 mt-6">
+            {steps.map((step, index) => {
+              if (index < 2) {
+                return;
+              }
+              return (
+                <div
+                  key={index}
+                  className={`flex items-center rounded ${getStepBackgroundColor(step.status)} px-4 py-3`}
+                >
+                  <div className="flex">
+                    <h1 className="font-medium text-sm">{index - 1}.</h1>
+                    <h1 className="font-medium text-sm ml-1">{step.name}</h1>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       )}
-
     </div>
   );
 }
